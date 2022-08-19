7c72f19008fd615512ebda1584b910f4472fa0dc
XumukBRAIN <xumuk_332@mail.ru>
2022-08-19 10:49:40 +0300

    done

--- src/main/resources/db/changelog/changeset/create-account-table.sql
+++ src/main/resources/db/changelog/changeset/create-account-table.sql
@@ -1,12 +1,20 @@
+--liquibase formatted sql
+--changeset Ivan Kudryashov:create_account_table
+
+CREATE
+    EXTENSION IF NOT EXISTS "uuid-ossp";
+
 create table account(
-    id uuid primary key default gen_random_uuid(),
-    account_number varchar(30) not null unique,
-    client_id uuid not null,
-    currency_code varchar(3),
-    current_balance numeric(19,4),
-    open_date date,
-    close_date date,
-    is_active boolean,
-    salary_project varchar(30),
-    blocked_sum numeric(19,4)
+                        id uuid primary key default uuid_generate_v4(),
+                        account_number varchar(30) not null unique,
+                        client_id uuid not null,
+                        currency_code varchar(3),
+                        current_balance numeric(19,4),
+                        open_date date,
+                        close_date date,
+                        is_active boolean,
+                        salary_project varchar(30),
+                        blocked_sum numeric(19,4)
 )
+
+--rollback DROP TABLE IF EXISTS account CASCADE
--- src/main/resources/db/changelog/changeset/create-card-table.sql
+++ src/main/resources/db/changelog/changeset/create-card-table.sql
@@ -1,12 +1,17 @@
+--liquibase formatted sql
+--changeset Ivan Kudryashov:create_card_table
+
 create table card (
-    id uuid primary key default gen_random_uuid(),
-    card_number char(16),
-    account_id uuid references account(id),
-    transactional_limit numeric(19,4),
-    status varchar(30),
-    expiration_date date,
-    holder_name varchar(50),
-    digital_wallet varchar(30),
-    is_default boolean,
-    card_product_id int references card_product(id)
+                      id uuid primary key default uuid_generate_v4(),
+                      card_number char(16),
+                      account_id uuid references account(id),
+                      transactional_limit numeric(19,4),
+                      status varchar(30),
+                      expiration_date date,
+                      holder_name varchar(50),
+                      digital_wallet varchar(30),
+                      is_default boolean,
+                      card_product_id int references card_product(id)
 )
+
+--rollback DROP TABLE IF EXISTS card CASCADE
