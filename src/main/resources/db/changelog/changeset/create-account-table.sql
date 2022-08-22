30aad5b76946d678f12889c9f2331ebc34b15175
XumukBRAIN <xumuk_332@mail.ru>
2022-08-22 15:33:39 +0300

    initial commit

--- src/main/resources/db/changelog/changeset/create-account-table.sql
+++ src/main/resources/db/changelog/changeset/create-account-table.sql
@@ -5,16 +5,16 @@ CREATE
     EXTENSION IF NOT EXISTS "uuid-ossp";
 
 create table account(
-                        id uuid primary key default uuid_generate_v4(),
-                        account_number varchar(30) not null unique,
-                        client_id uuid not null,
-                        currency_code char(3),
-                        current_balance numeric(19,4),
-                        open_date date,
-                        close_date date,
-                        is_active boolean,
-                        salary_project varchar(30),
-                        blocked_sum numeric(19,4)
+    id uuid primary key default uuid_generate_v4(),
+    account_number varchar(30) not null unique,
+    client_id uuid not null,
+    currency_code char(3),
+    current_balance numeric(19,4),
+    open_date date,
+    close_date date,
+    is_active boolean,
+    salary_project varchar(30),
+    blocked_sum numeric(19,4)
 )
 
 --rollback DROP TABLE IF EXISTS account CASCADE
--- src/main/resources/db/changelog/changeset/create-agreement-table.sql
+++ src/main/resources/db/changelog/changeset/create-agreement-table.sql
@@ -2,17 +2,17 @@
 --changeset Ivan Kudryashov:create_agreement_table
 
 create table agreement(
-                          id uuid primary key default uuid_generate_v4(),
-                          agreement_number varchar(20) not null unique,
-                          account_id uuid references account(id),
-                          product_id int references product(id),
-                          interest_rate numeric(6,4),
-                          start_date timestamp with time zone,
-                          end_date timestamp with time zone,
-                          initial_amount numeric(19,4),
-                          current_balance numeric(19,4),
-                          is_active boolean,
-                          auto_renewal boolean
+    id uuid primary key default uuid_generate_v4(),
+    agreement_number varchar(20) not null unique,
+    account_id uuid references account(id),
+    product_id int references product(id),
+    interest_rate numeric(6,4),
+    start_date timestamp with time zone,
+    end_date timestamp with time zone,
+    initial_amount numeric(19,4),
+    current_balance numeric(19,4),
+    is_active boolean,
+    auto_renewal boolean
 )
 
 --rollback DROP TABLE IF EXISTS agreement CASCADE
